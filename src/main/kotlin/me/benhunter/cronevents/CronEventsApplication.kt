5d71b049851706f2e9d564ffb18410ad6a181c1c
Ben Hunter <code@benhunter.me>
2023-08-20 15:07:41 -0500

    Add logging for SpringApplicationEvent

--- src/main/kotlin/me/benhunter/cronevents/CronEventsApplication.kt
+++ src/main/kotlin/me/benhunter/cronevents/CronEventsApplication.kt
@@ -1,11 +1,24 @@
 package me.benhunter.cronevents
 
+import org.slf4j.LoggerFactory
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.event.SpringApplicationEvent
 import org.springframework.boot.runApplication
+import org.springframework.context.event.EventListener
+import org.springframework.stereotype.Component
 
 @SpringBootApplication
 class CronEventsApplication
 
 fun main(args: Array<String>) {
-	runApplication<CronEventsApplication>(*args)
+    runApplication<CronEventsApplication>(*args)
+}
+
+@Component
+class SpringApplicationEventLogger {
+    private val logger = LoggerFactory.getLogger(javaClass)
+    @EventListener
+    fun listen(event: SpringApplicationEvent) {
+        logger.info("SpringApplicationEvent Recieved - $event")
+    }
 }
